854483db3df42fc0603f25d4d19098f832fce57b
Florencia Hilary <fhilary@oa-n5.msk.21-school.ru>
2020-07-02 13:01:44 +0300

    trying to rewrite mp_thinout

--- src/libmap/mp_thinout.c
+++ src/libmap/mp_thinout.c
@@ -18,32 +18,32 @@ static void	del(void *content, size_t size)
 		ft_memdel(&content);
 }
 
-static void	helper1(t_list **links)
-{
-	t_list	*ptr;
-	t_list	*todel;
+// static void	helper1(t_list **links)
+// {
+// 	t_list	*ptr;
+// 	t_list	*todel;
 
-	while ((*links) && (*links)->content_size == 0)
-	{
-		todel = *links;
-		*links = todel->next;
-		ft_lstdelone(&todel, del);
-	}
-	if (!*links)
-		return ;
-	ptr = *links;
-	while (ptr->next)
-	{
-		if (ptr->next->content_size == 0)
-		{
-			todel = ptr->next;
-			ptr->next = ptr->next->next;
-			ft_lstdelone(&todel, del);
-		}
-		else
-			ptr = ptr->next;
-	}
-}
+// 	while ((*links) && (*links)->content_size == 0)
+// 	{
+// 		todel = *links;
+// 		*links = todel->next;
+// 		ft_lstdelone(&todel, del);
+// 	}
+// 	if (!*links)
+// 		return ;
+// 	ptr = *links;
+// 	while (ptr->next)
+// 	{
+// 		if (ptr->next->content_size == 0)
+// 		{
+// 			todel = ptr->next;
+// 			ptr->next = ptr->next->next;
+// 			ft_lstdelone(&todel, del);
+// 		}
+// 		else
+// 			ptr = ptr->next;
+// 	}
+// }
 
 // static void	del(void *content, size_t size)
 // {
@@ -51,71 +51,56 @@ static void	helper1(t_list **links)
 // 		ro_free((t_room **)content);
 // }
 
-// static void	helper2(t_list **middle)
-// {
-// 	t_list	*ptr;
-// 	t_list	*todel;
-// 	int		flag;
-
-// 	flag = 1;
-// 	while (flag)
-// 	{
-// 		flag = 0;
-// 		while (!(*((t_room **)(*middle)->content))->links)
-// 		{
-// 			todel = *middle;
-// 			*middle = (*middle)->next;
-// 			ft_lstdelone(&todel, del);
-
-// 		}
-// 		ptr = *middle;
-// 		while (ptr->next)
-// 		{
-// 			if (!(*((t_room **)ptr->next->content))->links)
-// 			{
-// 				todel = ptr->next;
-// 				ptr->next = ptr->next->next;
-// 				ft_lstdelone(&todel, del);
-// 			}
-// 		}
-// 	}
-// }
-
-static void	walk(t_room *room)
+static void	helper(t_list **middle)
 {
 	t_list	*ptr;
+	t_list	*todel;
+	int		flag;
 
-
-	room->visited = 2;
-	ptr = room->links;
-	while (ptr)
+	flag = 1;
+	while (flag)
 	{
-		if ((*(t_room **)ptr->content)->visited != 2)
-			walk(*(t_room **)ptr->content);
-		if ((*(t_room **)ptr->content)->weight <= room->weight)
-			ptr->content_size = 0; // means that we should remove it later
-		ptr = ptr->next;
+		flag = 0;
+		while (!(*((t_room **)(*middle)->content))->links)
+		{
+			todel = *middle;
+			*middle = (*middle)->next;
+			ft_lstdelone(&todel, del);
+
+		}
+		ptr = *middle;
+		while (ptr->next)
+		{
+			if (!(*((t_room **)ptr->next->content))->links)
+			{
+				todel = ptr->next;
+				ptr->next = ptr->next->next;
+				ft_lstdelone(&todel, del);
+			}
+		}
 	}
 }
 
+
 void		mp_thinout(t_map *map)
 {
-	t_list	*ptr;
+	t_queue	*queue;
+	t_list	*u;
+	t_list	*v;
 
-	map->start->visited = 2;
-	ptr = map->start->links;
-	while (ptr)
+	queue = ft_quenew(&(map->start), sizeof(t_room *));
+	while (!ft_queisempty(queue))
 	{
-		walk(*(t_room **)ptr->content);
-		ptr = ptr->next;
+		u = ft_quepop(queue);
+		v = (*(t_room **)u->content)->links;
+		while (v)
+		{
+			if ((*(t_room **)v->content)->weight <= (*(t_room **)u->content)->weight)
+				v->content_size = 0;
+			v = v->next;
+		}
+		ft_lstdelone(&u, del);
 	}
-	walk(map->end);
-	ptr = map->middle;
-	while (ptr)
-	{
-		helper1(&((*(t_room **)ptr->content)->links));
-		ptr = ptr->next;
-	}
-	helper1(&(map->end->links));
-	// helper2(&(map->middle));
+	ft_quedel(&queue, del);
+	helper(&(map->middle));
 }
